17eb11a40a0b9f979a90a5a3cb2379c29727eea3
Felix Schneider <fsfuenf@gmail.com>
2013-11-16 22:07:35 +0100

    GUI-prototyp erweitert

--- docs/Prototyp/mainwindow.cpp
+++ docs/Prototyp/mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include "inspectorwindow.h"
 
+static QString def_string = "ffmpeg -i input.avi -b:v 64k -bufsize 64k output.avi";
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -18,4 +20,17 @@ void MainWindow::on_actionInspect_triggered()
 {
     InspectorWindow *wnd = new InspectorWindow();
     wnd->show();
+}
+
+void MainWindow::on_lineEdit_textEdited(const QString &arg1)
+{
+    ui->groupBox->setEnabled(false);
+    ui->pushButton->setEnabled(true);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->lineEdit->setText(def_string);
+    ui->groupBox->setEnabled(true);
+    ui->pushButton->setEnabled(false);
 }
--- docs/Prototyp/mainwindow.h
+++ docs/Prototyp/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,10 @@ public:
 
 private slots:
     void on_actionInspect_triggered();
+
+    void on_lineEdit_textEdited(const QString &arg1);
+
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
